ca20b751358145820d815b7089812043623e2aa1
Michael Webbers <michael@webbers.io>
2016-02-12 09:06:21 +0100

    Added enum class

---
+++ src/Enum.php
@@ -0,0 +1,108 @@
+<?php
+
+/**
+ * This file is part of the miBadger package.
+ *
+ * @author Michael Webbers <michael@webbers.io>
+ * @license http://opensource.org/licenses/Apache-2.0 Apache v2 License
+ * @version 1.0.0
+ */
+
+namespace miBadger\Enum;
+
+/**
+ * The enum class.
+ *
+ * @see http://tools.ietf.org/html/rfc5424
+ * @since 1.0.0
+ */
+class Enum
+{
+	/** @var int|double|string|boolean The value. */
+	private $value;
+
+	/**
+	 * Construct a enum with the given value.
+	 *
+	 * @param int|double|string|boolean $value
+	 * @throws \UnexpectedValueException
+	 */
+	public function __construct($value)
+	{
+		if (!in_array($value, static::toArray())) {
+			throw new \UnexpectedValueException($value . ' is not a valid enum value.');
+		}
+
+		$this->value = $value;
+	}
+
+	/**
+	 * Returns the string representation of the enum object.
+	 *
+	 * @return string the string representation of the enum object.
+	 */
+	public function __toString()
+	{
+		return $this->getName();
+	}
+
+	/**
+	 * Returns the ordinal.
+	 *
+	 * @return int the ordinal.
+	 */
+	public function getOrdinal()
+	{
+		$ordinal = array_search($this->value, array_values(static::toArray()));
+
+		return $ordinal !== false ? $ordinal : -1;
+	}
+
+	/**
+	 * Returns the name.
+	 *
+	 * @return string the name.
+	 */
+	public function getName()
+	{
+		$name = array_search($this->value, static::toArray());
+
+		return $name !== false ? $name : '';
+	}
+
+	/**
+	 * Returns the value.
+	 *
+	 * @return mixed the value;
+	 */
+	public function getValue()
+	{
+		return $this->value;
+	}
+
+	/**
+	 * Returns the enum objects.
+	 *
+	 * @return array<string,integer|double|string|boolean> the enum objects.
+	 */
+	public static function toArray()
+	{
+		return (new \ReflectionClass(\get_called_class()))->getConstants();
+	}
+
+	/**
+	 * Returns the enum object with the specified name.
+	 *
+	 * @param string $name
+	 * @return Enum the enum object with the specified name.
+	 * @throws \UnexpectedValueException
+	 */
+	public static function valueOf($name)
+	{
+		if (!defined('static::' . $name)) {
+			throw new \UnexpectedValueException($name . ' is not a valid enum name.');
+		}
+
+		return new static(constant('static::' . $name));
+	}
+}
---
+++ tests/EnumTest.php
@@ -0,0 +1,81 @@
+<?php
+
+/**
+ * This file is part of the miBadger package.
+ *
+ * @author Michael Webbers <michael@webbers.io>
+ * @license http://opensource.org/licenses/Apache-2.0 Apache v2 License
+ * @version 1.0.0
+ */
+
+namespace miBadger\Enum;
+
+/**
+ * The enum test class.
+ *
+ * @since 1.0.0
+ */
+class EnumTest extends \PHPUnit_Framework_TestCase
+{
+	/** @var The enum. */
+	private $enum;
+
+	public function setUp()
+	{
+		$this->enum = new EnumTestMock(EnumTestMock::TEST);
+	}
+
+	/**
+	 * @expectedException UnexpectedValueException
+	 */
+	public function test__construct()
+	{
+		new EnumTestMock('');
+	}
+
+	public function test__toString()
+	{
+		$this->assertEquals('TEST', $this->enum->__toString());
+	}
+
+	public function testGetOrdinal()
+	{
+		$this->assertEquals(0, $this->enum->getOrdinal());
+	}
+
+	public function testGetName()
+	{
+		$this->assertEquals('TEST', $this->enum->getName());
+	}
+
+	public function testGetValue()
+	{
+		$this->assertEquals('test', $this->enum->getvalue());
+	}
+
+	public function testToArray()
+	{
+		$this->assertEquals(['TEST' => 'test'], $this->enum->toArray());
+	}
+
+	public function testValueOf()
+	{
+		$this->assertEquals($this->enum, EnumTestMock::valueOf('TEST'));
+	}
+
+	/**
+	 * @expectedException UnexpectedValueException
+	 */
+	public function testValueOfFail()
+	{
+		$this->assertEquals($this->enum, EnumTestMock::valueOf(''));
+	}
+}
+
+/**
+ * The enum test mock class.
+ */
+class EnumTestMock extends Enum
+{
+	const TEST = 'test';
+}
